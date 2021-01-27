32260d599b7457b69e0c518e24967e3fbf766174
tomce <tomce@192.168.0.14>
2021-01-27 15:39:21 +0100

    "Tp 3"

--- src/entites/Personne.java
+++ src/entites/Personne.java
@@ -24,6 +24,52 @@ public class Personne {
 		fullAdress = adresse;
 	}
 	
+	
+	//---------------------GETTER-----------------------
+	
+	//Récupérer le prénom + nom en majuscule
+	public String getFullName() {
+		return lastname.toUpperCase() + " " + firstname;
+	}
+	
+	//Récupérer le nom
+	public String getLastname() {
+		return "Votre nom : " + lastname;
+	}
+	
+	//Récupérer le prénom
+	public String getFirstname() {
+		return "Votre prénom : " + firstname;
+	}
+	
+	//Récupérer l'adresse
+	public String getAdresse() {
+		return "Votre adresse : " + fullAdress;
+	}
+	
+	
+	//----------------------SETTER-----------------------
+	
+	//Modifié nom
+	public String setLastname(String nom) {
+		lastname = nom;
+		return "Votre nouveau prénom est : " + lastname;
+	}
+	
+	//Modifié prénom
+	public String setFirstname(String prenom) {
+		firstname = prenom;
+		return "Votre nouveau prénom est : " + firstname;
+	}
+	
+	//Modifié adresse
+	public String setAdress(AdressePostale adresse) {
+		fullAdress = adresse;
+		return "Votre nouvelle adresse est : " + fullAdress;
+	}
+	
+	
+	//----------------------toString-----------------------
 	public String toString(Personne p) {
 		
 		return "Je m'apelle " +lastname+ " " + firstname + ":\nVoici mon adresse : \n" + fullAdress ;
--- src/essais/TestPersonne.java
+++ src/essais/TestPersonne.java
@@ -23,9 +23,24 @@ public class TestPersonne {
 		
 		// instance de la classe Personne nommée p2
 		AdressePostale adresse1 = new AdressePostale(30, "Rue des machins", 44000, "Nantes");
+		AdressePostale adresse2 = new AdressePostale(26, "Rue de Clementville", 34070, "Montpellier");
 		Personne p2 = new Personne("Goupillot", "Etienne", adresse1);
 		
-		System.out.println(p2);
+		System.out.println(p2.getFullName());
+		System.out.println(p2.getFirstname());
+		System.out.println(p2.getLastname());
+		System.out.println(p2.getAdresse());
+		
+		System.out.println();
+		
+		p2.setLastname("Cermelli");
+		p2.setFirstname("Tom");
+		p2.setAdress(adresse2);
+		
+		System.out.println(p2.getFullName());
+		System.out.println(p2.getFirstname());
+		System.out.println(p2.getLastname());
+		System.out.println(p2.getAdresse());
 		
 		
 		/*p2.fullAdress = new AdressePostale(30, "Rue des machins", 44000, "Nantes");*/
