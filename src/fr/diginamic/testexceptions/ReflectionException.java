d61b6630942fd907d0b5fe782f2073b5aac1403a
tomce <tomce@192.168.0.14>
2021-02-09 11:34:01 +0100

    "Exception"

---
+++ src/fr/diginamic/testexceptions/ReflectionException.java
@@ -0,0 +1,12 @@
+package fr.diginamic.testexceptions;
+
+import java.lang.reflect.Field;
+
+public class ReflectionException extends Exception {
+
+	public ReflectionException(String message) {
+		super(message);
+		
+	}
+	
+}
--- src/fr/diginamic/testexceptions/TestReflectionUtils.java
+++ src/fr/diginamic/testexceptions/TestReflectionUtils.java
@@ -1,10 +1,19 @@
 package fr.diginamic.testexceptions;
 
+import listes.Ville;
+
 public class TestReflectionUtils {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+			throws IllegalArgumentException, IllegalAccessException, ReflectionException {
+		Ville ville = new Ville("Montpellier", 280_000);
+		try {
+			ReflectionUtils.afficherAttributs(null);
+		}
+
+		catch (ReflectionException e) {
+			System.err.println(e.getMessage());
+		}
 
 	}
-
 }
